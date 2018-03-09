1e71479cbb2b2db06497e41e8cd28f3d2cfe6e38
Aaron Magid <ahm64@case.edu>
2018-03-09 00:32:06 -0500

    feat(DurationMode): Hide / show duration field on edit page load.

--- control.lua
+++ control.lua
@@ -189,6 +189,7 @@ script.on_event(defines.events.on_gui_click, function(event)
 			CCContainer.editCombinatorContainer.typeButtonRow.CCToggleMode.state = (combinator.output.type == "toggle")
 			CCContainer.editCombinatorContainer.typeButtonRow.CCDurationMode.state = (combinator.output.type == "duration")
 			CCContainer.editCombinatorContainer.durationRow.editCombinatorDuration.text = combinator.output.duration
+			CCContainer.editCombinatorContainer.durationRow.style.visible = (combinator.output.type == "duration")
 		else
 			combinator.output = {
 				signalNum = 1,
