29c6f969693e428fa00e8d8b5b674269d715e04b
Aaron Magid <ahm64@case.edu>
2018-03-09 00:51:09 -0500

    fix(DurationMode): Fixed spelling error.

--- control.lua
+++ control.lua
@@ -436,7 +436,7 @@ function createGUI(player)
 	typeButtonRow.add{type="radiobutton", state=true, name="CCToggleMode", caption="Toggle Mode", tooltip="When in this mode, a Control Combinator must be manually turned on and off from the GUI."}
 	typeButtonRow.add{type="radiobutton", state=false, name="CCDurationMode", caption="Duration Mode", tooltip="When in this mode, a Control Combinator will automatically turn off after the specified amount of time."}
 
-	local durationRow = editCombinatorComtainer.add{type="flow", direction="vertical", name="durationRow"}
+	local durationRow = editCombinatorContainer.add{type="flow", direction="vertical", name="durationRow"}
 	setStyles(durationRow, {
 		visible = false
 	})
